5d868b8d207097739b0304dacb0da2c98d15689f
Christopher-Tan <72955765+Christopher-Tan@users.noreply.github.com>
2021-10-20 08:50:21 -0400

    Created list.h file

---
+++ list.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct song_node {
+    char name[100];
+    char artist[100];
+    struct song_node *next;
+};
+
+void print_list(struct song_node *front);
+struct song_node * insert_front(struct song_node *front, char name[], char artist[]);
+struct song_node * free_list(struct song_node *front);
+struct song_node * remove_node(struct song_node *front, char name[], char artist[]);
